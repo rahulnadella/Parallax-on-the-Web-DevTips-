1efc2cdf1fdd86999ee8126757d8b455a18d6027
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-07 13:47:23 -0400

    Initial commit

--- GruntFile.js
+++ GruntFile.js
@@ -37,6 +37,7 @@ module.exports = function(grunt)
   });
 
 
+  grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
