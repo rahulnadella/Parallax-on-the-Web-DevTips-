13cfd62cf31974eadb74181f881ccdff359c1fca
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-07 12:48:33 -0400

    Initial Commit

--- GruntFile.js
+++ GruntFile.js
@@ -1,7 +1,33 @@
 module.exports = function(grunt)
 {
-  grunt.registerTask('speak', function()
-  {
-    console.log("I'm speaking");
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';',
+      },
+      js: {
+        src: ['js/**/*.js'],
+        dest: 'dist/js/built.js',
+      },
+      css: {
+        src: ['css/**/*.css'],
+        dest: 'dist/css/style.css',
+      },
+    },
+    watch: {
+      js: {
+        files: ['js/**/*.js'],
+        tasks: ['concat:js'],
+      },
+      css: {
+        files: ['css/**/*.css'],
+        tasks: ['concat:css'],
+      },
+    },
   });
+
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['concat', 'watch']);
 }
